8b3ee8a7e8ea754fae48358ac94539113145447a
Neil Redman <nredman@uwaterloo.ca>
2013-02-06 11:17:31 -0600

    Added Clear analysis button

--- Server/Client/md_analysis.js
+++ Server/Client/md_analysis.js
@@ -59,8 +59,8 @@ Analysis.method("onSelectionChanged", function(list){
     // get the products that are missing to make up the complete set.
     var missingProducts = originalData.getMissingProductsInCommonData(data.getCommon(false), list);
   
-  
-    var label = "[Complete] - The set is a complete concept";
+    var clearButton = '<button id="clearAnalysis">Clear</button><br>';
+    var label = clearButton;
     
     if (commonFeatures.length > 0)
     {    
@@ -68,21 +68,27 @@ Analysis.method("onSelectionChanged", function(list){
         {
             if (missingProducts.length <= 10) // reasonable to add products to make the set complete
             {
-                var addButton = '<button id="addMissing">add</button>'
-                label = "[Not Complete], " + addButton + " [" + missingProducts.toString() + "]";
+                var addButton = '<button id="addMissing">add</button>';
+                label += "[Not Complete], " + addButton + " [" + missingProducts.toString() + "]";
             }
             else
             {
-                var addButton = '<button id="addMissing">add</button>'
-                label = "[Not Complete], should " + addButton + " more than 10 products...";
+                var addButton = '<button id="addMissing">add</button>';
+                label += "[Not Complete], should " + addButton + " more than 10 products...";
             }
             
         }
+        else
+        {
+            label += '[Complete] - The set is a complete concept';
+        }
     }
     else
-        label = "Please select more products for analysis";
+        label += "Please select more products for analysis";
 
     $("#analysis #completeness").html(label);
+
+// add function to addMissing button
     if($("#addMissing")){
         $("#addMissing").click(function(){
             var i;
@@ -91,6 +97,15 @@ Analysis.method("onSelectionChanged", function(list){
             }
         });
     }
+
+// add function for clear button
+    $("#clearAnalysis").click(function(){
+        var selected = host.selector.selection;
+        while (selected.length > 0){
+            host.selector.onDeselected(selected[selected.length-1]);
+            selected.pop();
+        };
+    });
     
 //    commonData.products[0] = label;
     
